4446be2ed2dcc370a26aea0722b3e6aa6d93e5a2
rosnerdev <yrosner123@gmail.com>
2022-10-01 14:22:58 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Bb-code-block
+This is a readme for your new Budibase plugin.
+
+# Description
+Display your code in budibase no matter what language your code is written in.
+
+Find out more about [Budibase](https://github.com/Budibase/budibase).
+
+## Instructions
+
+To build your new  plugin run the following in your Budibase CLI:
+```
+budi plugins --build
+```
+
+You can also re-build everytime you make a change to your plugin with the command:
+```
+budi plugins --watch
+```
+
---
+++ src/Component.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { getContext } from "svelte"
+  import { HighlightAuto } from "svelte-highlight";
+  import atom from "svelte-highlight/styles/atom-one-dark";
+  import atomLight from "svelte-highlight/styles/atom-one-light";
+
+  export let code
+  export let theme = "dark"
+
+  const { styleable } = getContext("sdk")
+  const component = getContext("component")
+</script>
+
+<svelte:head>
+{#if theme.toLowerCase() == "light"}  
+  {@html atomLight}
+{:else}
+  {@html atom}
+{/if}
+</svelte:head>
+
+<div use:styleable={$component.styles}>
+  <HighlightAuto bind:code/>
+</div>
